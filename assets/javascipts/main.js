982738700842128c9f65a292500ffaaeac8c16e0
Ewuola olatunde Ajibayo <olatundeewuola@gmail.com>
2021-03-11 16:37:41 +0100

    Add files via upload

---
+++ assets/javascipts/main.js
@@ -0,0 +1,8 @@
+// swithing from splash screen to next page after animation is done
+var timePeriodInMs = 12000;
+
+setTimeout(function()
+    {
+        document.getElementById("splashScreen").style.display = "none";
+    },
+    timePeriodInMs);
